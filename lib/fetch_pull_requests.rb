a1d822a5369101aff29527450a252ef6f839ae49
Ben Forrest <ben@silvabox.com>
2015-09-25 14:27:50 +0100

    builds first version of gem - no run tests yet

---
+++ lib/fetch_pull_requests.rb
@@ -0,0 +1,69 @@
+require 'octokit'
+
+module MakersToolbelt
+  class FetchPullRequests
+    OPEN_SWITCHES = ['-a', '--all', '-o', '--open']
+    CLOSED_SWITCHES = ['-a', '--all', '-c', '--closed']
+
+    attr_reader :options, :filter
+
+    def initialize(options = {})
+      @options = options
+      @filter = options[:filter] || '--open'
+    end
+
+    def run()
+      pulls = []
+      pulls << open_pull_requests if include_open?
+      pulls << closed_pull_requests if include_closed?
+
+      pulls.flatten!.select! {|p| p.head.repo }
+
+      pulls.map { |p| [p.head.repo.owner.login, p.head.repo.html_url] }.each do |name, url|
+        puts "adding #{name}"
+        `git remote add #{name} #{url}`
+        puts "fetching #{url}"
+        `git fetch #{name}`
+      end
+    end
+
+    def repo
+      @repo ||= repo_from_remote_path(origin_path)
+    end
+
+    def repo_from_remote_path(path)
+      @regex ||= /\s*(?:Fetch URL: )?(?:https:\/\/|git@)github\.com(?::|\/)?(.*\/.*).git/
+      @regex.match(path).captures.first
+    end
+
+    def include_open?
+      OPEN_SWITCHES.include? filter
+    end
+
+    def include_closed?
+      CLOSED_SWITCHES.include? filter
+    end
+
+    private
+
+    def open_pull_requests
+      puts "fetching open pull requests from #{repo}..."
+      client.pull_requests repo, state: 'open', per_page: 100
+    end
+
+    def closed_pull_requests
+      puts "fetching closed pull requests from #{repo}..."
+      pulls = client.pull_requests repo, state: 'closed', per_page: 100
+      pulls.concat client.last_response.rels[:next].get.data
+    end
+
+    def origin_path
+      @origin_path ||= `git remote show origin | grep 'Fetch URL:'`
+    end
+
+    def client
+      @client ||= Octokit::Client.new client_id: ENV['GITHUB_CLIENT_ID'], client_secret: ENV['GITHUB_CLIENT_SECRET']
+    end
+  end
+
+end
--- makers_toolbelt.gemspec
+++ makers_toolbelt.gemspec
@@ -1,6 +1,12 @@
 Gem::Specification.new do |s|
-  s.name 'makers_toolbelt'
-  s.version '0.0.1'
-  s.files ['lib/makers_toolbelt.rb']
+  s.name = 'makers_toolbelt'
+  s.version = '0.0.1'
+  s.summary = 'Makers Academy command toolbelt'
+  s.authors = ['Ben Forrest', 'Sam Joseph']
+  s.files = [
+    'lib/makers_toolbelt.rb',
+    'command_map.yml',
+    'lib/makers_toolbelt/fetch_pull_requests.rb'
+  ]
   s.executables << 'makers'
 end
